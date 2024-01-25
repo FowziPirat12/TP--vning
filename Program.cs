a6b95a4000ff6139df570407b4e661f5796acb0f
FowziPirat12 <greger.grym@gmail.com>
2024-01-25 11:27:36 +0100

    tryparse

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System; 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int resultat;
+        
+        if(!int.TryParse(Console.ReadLine(), out resultat))
+        Console.WriteLine("Skriv in en Siffra");
+        else 
+        {
+            Console.WriteLine(resultat);
+        }
+
+        bool TryParse(string s, out int resultat)
+        {
+            resultat = int.Parse(s);
+            
+        }
+
+    }
+}
